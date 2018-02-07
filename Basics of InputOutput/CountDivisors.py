b6d747cfbe17a11132b19e580a9be1a5e1056ef8
rpiganet <rpiganet@gmail.com>
2018-02-07 05:38:49 -0500

    message

--- Basics of InputOutput/CountDivisors.py
+++ Basics of InputOutput/CountDivisors.py
@@ -8,3 +8,11 @@ print('Hi, %s.' % name)         # Writing output to STDOUT
 '''
 
 # Write your code here
+values_in = input().split()
+
+try:
+  L, R, K = values_in[0], values_in[1], values_in[2]
+  print("ok")
+except:
+  print("ERROR")
+
